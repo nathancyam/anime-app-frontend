e09859f8387fefe8c5f85ca124f0297bcc3fa61b
Nathan Yam <nathancyam@gmail.com>
2016-03-11 17:45:39 +1100

    Make anime collection section consistently named

--- app/containers/AnimeCollection.js
+++ app/containers/AnimeCollection.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import AnimeList from '../views/AnimeList';
+import AnimeCollection from '../views/AnimeCollection';
 import Immutable from 'immutable';
 import {
   fetchAnime,
@@ -45,4 +45,4 @@ const mapDispatchToProps = (dispatch) => {
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(AnimeList);
+)(AnimeCollection);
--- app/routes.js
+++ app/routes.js
@@ -3,17 +3,17 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import Layout from './views/Layout';
 import TorrentList from './containers/TorrentList';
 import Settings from './views/Settings';
-import AnimeList from './containers/AnimeList';
+import AnimeCollection from './containers/AnimeCollection';
 import AnimeItem from './containers/AnimeItem';
 import Login from './containers/Login';
 
 module.exports = (
   <Route path="/" component={Layout}>
     <Route path="/torrents" name="torrents" component={TorrentList} />
-    <Route path="/anime" name="anime" component={AnimeList} />
+    <Route path="/anime" name="anime" component={AnimeCollection} />
     <Route path="/anime/:animeId" name="anime_item" component={AnimeItem} />
     <Route path="/settings" component={Settings} />
     <Route path="/login" component={Login} />
-    <IndexRoute name="index" component={AnimeList} />
+    <IndexRoute name="index" component={AnimeCollection} />
   </Route>
 );
