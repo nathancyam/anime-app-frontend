970928d8777d6682566589780afa7fd5ce88919a
Nathan Yam <nathancyam@gmail.com>
2017-06-18 17:26:52 +1000

    Further SW fetch event handlers

--- app/sw.js
+++ app/sw.js
@@ -1,33 +1,85 @@
 const staticCacheName = 'anime-app-3';
+const contentCacheName = 'anime-app-content-1';
+
+const latestCaches = [ staticCacheName, contentCacheName ];
 
 self.addEventListener('install', event => {
-
+  event.waitUntil(
+    caches.open(staticCacheName)
+      .then(cache => {
+        return cache.addAll([
+          '//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css',
+        ]);
+      })
+  )
 });
 
 self.addEventListener('activate', event => {
-
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => Promise.all(
+        cacheNames.filter(name => name.includes('anime-app') && !latestCaches.includes(name))
+          .map(deleteCacheName => caches.delete(deleteCacheName))
+      )
+    )
+  )
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cacheEntry => {
-      return cacheEntry || fetch(event.request)
-        .then(response => {
-          return caches.open(staticCacheName)
-            .then(cache => {
-              const { url, method } = event.request;
-              if (method !== 'GET') {
-                return response;
-              }
-
-              if (url.includes('/api') || url.includes('.png') || url.includes('.jpg')) {
-                cache.put(event.request, response.clone());
-              }
-              return response;
-            })
+function serveImage(request) {
+  return caches.open(contentCacheName)
+    .then(cache => {
+      return cache.match(request)
+        .then(res => {
+          return {res, cache};
         });
     })
-  );
+    .then(({res, cache}) => {
+      return res || fetch(request)
+          .then(res => {
+            cache.put(request, res.clone());
+            return res;
+          });
+    });
+}
+
+function serveApi(request) {
+  return caches.open(staticCacheName)
+    .then(cache => {
+      return cache.match(request)
+        .then(res => {
+          return {res, cache};
+        });
+    })
+    .then(({res, cache}) => {
+      return res || fetch(request)
+          .then(res => {
+            cache.put(request, res.clone());
+            return res;
+          });
+    });
+}
+
+self.addEventListener('fetch', ({ request } = event) => {
+  const requestUrl = new URL(request.url);
+  const cacheableAPIUrls = [
+    '/api/ann/'
+  ];
+
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname.startsWith('/media/images/')) {
+      event.respondWith(serveImage(request));
+      return;
+    }
+
+    if (
+      event.request.method === 'GET' &&
+      cacheableAPIUrls.filter(url => requestUrl.pathname.startsWith(url)).length !== 0
+    ) {
+      event.respondWith(serveApi(request));
+    }
+  }
+
+  event.respondWith(fetch(request));
 });
 
 self.addEventListener('message', event => {
